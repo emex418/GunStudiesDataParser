61ab2cada5c51fa7fa88f9ef8690defea3172531
emex418 <elizabeth@eckeshome.com>
2019-03-19 11:10:20 -0700

    added methods to Utils class - 1/3 implementation

--- src/Utils.java
+++ src/Utils.java
@@ -1,4 +1,31 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Utils {
     //TODO; convert between strings and files
     //Take out weird commas
+
+    public static String readFileAsString(String filepath) {
+        StringBuilder output = new StringBuilder();
+
+        try (Scanner scanner = new Scanner(new File(filepath))) {
+
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                output.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+
+    private static String fixDataIrregularities(String dataArray) {
+        //TODO: fill method
+      return "";
+    }
+    private static void updateFileFromString(String s, String filepath){}
+
 }
