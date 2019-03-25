e48f902fbf94c59d1a2dc8e0628722f4deee64e7
emex418 <elizabeth@eckeshome.com>
2019-03-25 09:59:29 -0700

    Fix merging errors - all changes from 3/25 class

--- src/EducationData.java
+++ src/EducationData.java
@@ -1,6 +1,37 @@
 public class EducationData {
-    //TODO: have all variables that are stored in the education Data
-    //columns L, M, N, O, T, U, V, W
-    public EducationData(String data){}
+    //variables extract percentages of people in the county that had a certain level of education from 2012 to 2016
+    //someCollege is 1 to 3 years while more college is 4 or more years
+    double noHighSchool, highSchool, someCollege, moreCollege;
+
+    //columns L, M, N, O, T, U, V, W << 1970 and 1980 variables but we parsed 2016 variables because it more aligns with the other data
+    public EducationData(String data) {
+        parseEduData(data);
+    }
+
+    private void parseEduData(String data) {
+        String[] vars = data.split(",");
+        int lastIndex = vars.length - 1;
+        noHighSchool = Double.parseDouble(vars[lastIndex - 3]);
+        highSchool = Double.parseDouble(vars[lastIndex - 2]);
+        someCollege = Double.parseDouble(vars[lastIndex - 1]);
+        moreCollege = Double.parseDouble(vars[lastIndex]);
+    }
+
+    public double getNoHighSchool() {
+        return noHighSchool;
+    }
+
+    public double getHighSchool() {
+        return highSchool;
+    }
+
+    public double getSomeCollege() {
+        return someCollege;
+    }
+
+    public double getMoreCollege() {
+        return moreCollege;
+    }
+
 
 }
